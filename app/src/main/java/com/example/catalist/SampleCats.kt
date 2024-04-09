993526c35f974a30bec788dd0ebc337595d66fac
milanKosanovic <mkosanovic5820rn@raf.rs>
2024-04-09 13:31:35 +0200

    prvi comit

---
+++ app/src/main/java/com/example/catalist/SampleCats.kt
@@ -0,0 +1,26 @@
+package com.example.catalist
+
+val SampleCats  = listOf(
+
+    CatData(
+        id = "1",
+        raceName = "Scotish",
+        details = "Race that love to climb."
+    ),
+    CatData(
+        id = "2",
+        raceName = "Britanska",
+        details = "Short haired cat with a long tail."
+    ),
+    CatData(
+        id = "3",
+        raceName = "Sphynx",
+        details = "Whithout a hair at all."
+    ),
+    CatData(
+        id = "4",
+        raceName = "Persian",
+        details = "Gods above all other races"
+    ),
+
+)
